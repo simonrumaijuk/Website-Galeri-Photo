5ac57dd181162311f0cf9bf1dae3519317c305b9
simonrumaijuk <164398921+simonrumaijuk@users.noreply.github.com>
2024-03-22 01:34:07 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Website Galeri Foto</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+</head>
+<body class="bg-secondary">
+<nav class="navbar navbar-expand-lg bg-info">
+  <div class="container">
+    <a class="navbar-brand" href="index.php">Website Galeri Simon</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse mt-2" id="navbarNavAltMarkup">
+      <div class="navbar-nav me-auto">
+        
+      </div>
+      <a href="register.php" class="btn btn-outline-dark m-1">Daftar</a>
+      <a href="login.php" class="btn btn-outline-dark m-1">Masuk</a>
+    </div>
+  </div>
+</nav>
+
+<div class="container mt-3">
+    <div class="row">
+        <div class="col">
+            <div class="col-md-3">
+                    <img src="simon.JPG" title="" style="height:500px" width="1100px">
+                    <div class="card-footer text-center">
+                    </div>
+                </div>
+            </div>   
+    </div>
+</div>
+
+<footer class="d-flex justify-content-center border-top mt-3 bg-warning fixed-bottom">
+    <p>&copy; UKK RPL 2024 | Simon Vega Rumaijuk</p>
+</footer>
+    
+<script type="text/javascript" src="assets/css/bootstrap.min.js"></script>
+</body>
+</html>
